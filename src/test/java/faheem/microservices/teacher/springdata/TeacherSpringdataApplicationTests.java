324f222018665dda8daf281f45fe114b9b2560f0
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2023-09-28 14:37:27 +0200

    main file

---
+++ src/test/java/faheem/microservices/teacher/springdata/TeacherSpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package faheem.microservices.teacher.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherSpringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
